437cbf1c3b1d7b7550e6b849bb816876a0e707f2
Kakurenboshi <lordalchimiste@hotmail.fr>
2023-07-09 22:56:01 +0200

    [main] 🚚 Renamed App to Root

--- portfolio/src/index.tsx
+++ portfolio/src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './components/app/App';
+import Root from './components/root/Root';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <Root/>,
   },
 ]);
 
