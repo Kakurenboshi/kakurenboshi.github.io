15b9babbb1e7b99d68a9efe71d488c5fb2e48cc0
Kakurenboshi <lordalchimiste@hotmail.fr>
2023-07-11 20:25:30 +0200

    [main] 🚀 Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,22 @@
 #!/bin/bash -e
 
-shopt -s extglob
-
 yarn build
-rm -r !(".git"|"build"|"node_modules") 
-mv build/* .
-rm -r build
-echo "/node_modules" > .gitignore
-git add .
+
+BUILD_DIR=`mktemp -d`
+mv build "$BUILD_DIR"
+mv node_modules "$BUILD_DIR"
+
 git checkout deploy
+
+rm -rf *
+mv "$BUILD_DIR/build/"* .
+echo "/node_modules" > .gitignore
+
+git add .
 git commit -m "🚀 Version $(date +%s)"
 git push
+
 git checkout main
+mv "$BUILD_DIR/node_modules" .
+
+rm -rf "$BUILD_DIR"
