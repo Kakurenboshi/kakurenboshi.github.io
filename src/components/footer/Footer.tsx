c128587c37610ae24066c6d22c232ead57f4f248
Kakurenboshi <lordalchimiste@hotmail.fr>
2023-07-26 18:21:38 +0200

    [main] 💄 Improve header, content and footer UX

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import s from './Footer.module.scss';
 
 function Footer() {
+  let prevScrollpos = window.pageYOffset;
+
+  function hideOnScroll() {
+    const currentScrollPos = window.pageYOffset;
+    const footer = document.querySelector('footer > .pure-menu');
+
+    if (prevScrollpos > currentScrollPos) {
+      footer?.classList.add(s.Show);
+      footer?.classList.remove(s.Hide);
+    } else {
+      footer?.classList.add(s.Hide);
+      footer?.classList.remove(s.Show);
+    }
+
+    prevScrollpos = currentScrollPos;
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', hideOnScroll);
+    return () => window.removeEventListener('scroll', hideOnScroll);
+  });
+
   return (
     <footer className={s.Content}>
-      <div className='pure-menu pure-menu-horizontal pure-menu-scrollable'>
+      <div className={`pure-menu pure-menu-horizontal pure-menu-scrollable ${s.Show}`}>
         <div className={s.PureMenuHeadingWrapper}>
           <a href='#' className='pure-menu-heading pure-menu-link'>
             © Kakurenboshi 2023
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import s from './Header.module.scss';
 
 function Header() {
+  let prevScrollpos = window.pageYOffset;
+
+  function hideOnScroll() {
+    const currentScrollPos = window.pageYOffset;
+    const header = document.querySelector('header > .pure-menu');
+
+    if (prevScrollpos > currentScrollPos) {
+      header?.classList.add(s.Show);
+      header?.classList.remove(s.Hide);
+    } else {
+      header?.classList.add(s.Hide);
+      header?.classList.remove(s.Show);
+    }
+
+    prevScrollpos = currentScrollPos;
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', hideOnScroll);
+    return () => window.removeEventListener('scroll', hideOnScroll);
+  });
+
   return (
     <header className={s.Content}>
-      <nav className='pure-menu pure-menu-horizontal pure-menu-scrollable'>
+      <nav className={`pure-menu pure-menu-horizontal pure-menu-scrollable ${s.Show}`}>
         <ul className='pure-menu-list'>
           <li className='pure-menu-item'>
             <Link className='pure-menu-link' to='/'>
